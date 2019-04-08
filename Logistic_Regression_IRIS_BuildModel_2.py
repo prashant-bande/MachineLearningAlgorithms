17c1de4ec0cac0784959bec493619eea71ab5a0b
prashant-bande <45324633+prashant-bande@users.noreply.github.com>
2019-04-08 20:10:50 +0530

    Add files via upload

---
+++ Logistic_Regression_IRIS_BuildModel_2.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 23 17:42:08 2019
+
+@author: saanvi
+"""
+# Import the libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Import dataset
+col_name = ['sepal length', 'sepal width', 'petal length', 'petal width', 'species']
+dataset = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', names=col_name)
+dataset['species'] = dataset['species'].str.split('-').str.get(1)
+X = dataset.iloc[:,:-1]
+y = dataset.iloc[:,-1]
+
+# Handeling categorical data
+from sklearn.preprocessing import LabelEncoder
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+# Generating training and test data
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Build a Model 
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state=0)
+classifier.fit(X_train, y_train)
+
+# Save model
+from sklearn.externals import joblib
+joblib.dump(classifier, 'IRIS_LGR.pkl')
+from_joblib = joblib.load('IRIS_LGR.pkl')
+
+# Prediction
+y_pred = from_joblib.predict(X_test)
+
+# Accuracy check 
+from sklearn.metrics import confusion_matrix
+confusion_matrix(y_test, y_pred)
+
---
+++ Simple_Linear_Regression.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 23 08:32:47 2019
+
+@author: ab59349
+"""
+
+# Importing the libraries and dataset
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:,:-1]
+y = dataset.iloc[:,-1]
+
+# Taking care of  missing data
+"""from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
+imputer = imputer.fit(X.iloc[:,1:3])
+X.iloc[:,1:3] = imputer.transform(X.iloc[:,1:3]).astype(int)"""
+
+# Encoding categorical data
+# Encoding independent variable
+"""from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X.iloc[:,0] = labelencoder_X.fit_transform(X.iloc[:,0])
+onehotencoder = OneHotEncoder(categorical_features=[0])
+X = onehotencoder.fit_transform(X).toarray()
+# Encoding dependent variable
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+# Avoiding dummy variable trap
+X = X[:, 1:]"""
+
+# Import SimpleLinearRegression from sklearn
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+# Fit the model to training data set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Prediction using the model on test dataset
+y_pred = regressor.predict(X_test)
+
+
+# Check model accuracy using r2_score
+from sklearn.metrics import r2_score
+r2_score(y_test,y_pred)
+
+# Visualizing the training set
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Training Set')
+plt.ylabel('Salary')
+plt.xlabel('Experience')
+plt.show()
+
+# Visualizing the test set
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Test Set')
+plt.ylabel('Salary')
+plt.xlabel('Experience')
+plt.show()
+
+
+
