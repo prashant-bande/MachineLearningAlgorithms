115366e04ac32de3a90e191118016ad782873451
prashant-bande <prince.bande@gmail.com>
2019-04-12 12:18:23 +0530

    Update Clf_KNeighborClassification_SocialNetworkAds.py

--- Clf_KNeighborClassification_SocialNetworkAds.py
+++ Clf_KNeighborClassification_SocialNetworkAds.py
@@ -34,7 +34,6 @@ classifier.fit(X_train, y_train)
 # Predicting test result
 y_pred = classifier.predict(X_test)
 
-
 # Evaluate the Result
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
